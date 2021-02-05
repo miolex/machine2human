75edf822d7c8cd34e9dffc725ae8408af7f96ebd
miolex <mioleh666@gmail.com>
2021-02-05 17:30:21 +0200

    Update hum2sec and hum2sec_test

--- hum2sec.go
+++ hum2sec.go
@@ -26,14 +26,29 @@ var stringToSec = map[string]int{
 
 // Hum2Sec implement convertation readable string to seconds
 func Hum2Sec(input string) int {
-	var seconds int
+	// if string just is number for example "1332"
+	x, err := strconv.Atoi(input)
+	if err == nil {
+		return x
+	}
 
+	var seconds int
 	input = strings.TrimSpace(input)
 	r := regexp.MustCompile(`(\d+)+\s*([yMwdhmsглМндчмс])`)
-	matched := r.FindAll([]byte(input), -1)
 
+	matched := r.FindAll([]byte(input), -1)
 	for _, m := range matched {
 		temp := strings.Fields(string(m))
+		if len(temp) < 2 {
+			for i := range stringToSec {
+				if strings.Contains(temp[0], i) {
+					tmp := string(temp[0])
+					tmp = strings.ReplaceAll(tmp, i, " "+i)
+					temp = strings.Fields(tmp)
+				}
+			}
+		}
+
 		t, _ := strconv.Atoi(temp[0])
 		seconds += t * stringToSec[temp[1]]
 	}
